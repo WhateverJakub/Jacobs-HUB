9e83ca0235e4b10db9cd7a1e7e6aba899e08052a
Jakub <70804792+WhateverJakub@users.noreply.github.com>
2023-04-29 18:37:48 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/GUI.lua')))()
-local Window = OrionLib:MakeWindow({Name = "Jakub's (FPS Unlocker mine only) scripts", HidePremium = false, IntroText = "FPS Unlocker", SaveConfig = false, ConfigFolder = "FPSULL"})
+local Window = OrionLib:MakeWindow({Name = "Powering Exploits Hub", HidePremium = false, IntroText = "Powering Exploits | Counter Blox", SaveConfig = false, ConfigFolder = "FPSULL"})
 
 local FPSULLTAB = Window:MakeTab({
 	Name = "FPS Unlocker",
@@ -19,3 +19,16 @@ FPSULLTAB:AddButton({
     loadstring(game:HttpGet("https://raw.githubusercontent.com/WhateverJakub/FPS-Unlocker/main/FPSUnlockerPart2"))() 
   	end    
 })
+
+local Legit = Window:MakeTab({
+	Name = "Legit",
+	Icon = "rbxassetid://13289895366",
+	PremiumOnly = false
+})
+Legit:AddToggle({
+	Name = "Silent Aim",
+	Default = false,
+	Callback = function(Value)
+                loadstring(game:HttpsGet("https://raw.githubusercontent.com/WhateverJakub/Jacobs-HUB/main/SilentAim.lua"))()
+		print(Value)
+	end    
